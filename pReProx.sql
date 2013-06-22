c7ba155c8ea16a64b6e5a6d0573e1c72623db7bd
S0lll0s <SMOArena@gmx.de>
2013-06-22 16:23:43 +0200

    Removed pReProx.sql, added pReProx.db instead

--- pReProx.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS `ports` (
-  `port` int(11) NOT NULL,
-  `expires` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`port`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
